cc0352da69a757d5c83dd330cfe12403e0b826bb
Rodrigo "kn" Garcia <78116445+rogarcia7@users.noreply.github.com>
2025-09-30 20:59:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/usuario', (req, res) => {
   res.json({ usuario: 'rogarcia7' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
